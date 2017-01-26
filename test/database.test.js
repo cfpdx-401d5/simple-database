e3536421aa93a32701692defb7e44d78189e4ba2
Brigitte Huneke <brigittehuneke@gmail.com>
2017-01-26 13:02:30 -0800

    tests to check/create database directory

--- test/database.test.js
+++ test/database.test.js
@@ -3,6 +3,26 @@ const assert = require('assert');
 const fs = require('fs');
 const dbCreate = database.create('./test-dir');
 
+describe('directory exists', done => {
+    it('checks that the directory does not exist', function(done) {
+        fs.readdir('./test/test-dir-create', (err, files) => {
+            if(err) return done(err);
+            assert.ifError(null);//passes when exists
+
+            done(); 
+        });
+    });
+    it('creates directory', function() {
+        database.create('./test/test-dir-create');
+        fs.readdir('./test/test-dir-create', (err,files) => {
+            if(err) return done(err);
+            assert.equal(files, []);
+
+            done();
+        });
+    })
+});
+
 describe('simple directory app', function() {
     it('gets all file contents', done => {
         dbCreate.getAll('./test/test-dir', (err, contents) => {
