58b1dc92f757edc0bf9b017fd4141e6853745d98
Lynn <code@krogseng.com>
2017-01-30 10:24:19 -0800

    Create DB object, some test cod e added.

--- test/create-db.test.js
+++ test/create-db.test.js
@@ -4,14 +4,26 @@ const createDb = require('../lib/create-db');
 const fs = require('fs');
 const path = require('path');
 const mkdirp = require('mkdirp');
+const rimraf = require('rimraf');
 var obj = { "cat": "kitty" };
+var cb = function() {
+    console.log('Problem with file removal');
+};
 
 describe('create a directory for database', function() {
-    it('create new db and first object', done => {
+    it('create new db object', done => {
         createDb('pickle', obj, (err, newDir) => {
             if (err) return done(err);
             assert.deepEqual(newDir, 'pickle');
         });
         done();
+    });
+
+    it('save db obj ', function() {
+
+    });
+
+    after(function() {
+        rimraf('./pickle', cb);
     });
 });
