087ef38134a35434531a4c4e90b84c54dc9d6b1e
Brigitte Huneke <brigittehuneke@gmail.com>
2017-01-31 10:02:56 -0800

    saves file passing

--- test/db-server.test.js
+++ test/db-server.test.js
@@ -7,3 +7,53 @@ const rimraf = require('rimraf');
 const PORT = 65000;
 const TEST_DIR = './test/test-dir-create';
 
+describe('db server', () => {
+    before(done => {
+        rimraf(TEST_DIR, () => done());
+    });
+
+    before(done => {
+        server.start({
+            baseDir: TEST_DIR,
+            port: PORT },
+        () => { done();}
+        );
+    });
+
+    after( done => {
+        client.end(done);
+        server.stop();
+    });
+
+    let client;
+
+    before(done => {
+        client = net.connect({ port: PORT }, () => {
+            client.setEncoding('utf8');
+            done();
+        })
+    });
+
+    let saved;
+
+    it('client saves a "file"', done => {
+        const message = {
+            method: 'save',
+            table: 'names',
+            data: {
+                name: 'sam'
+            }
+        };
+        
+        client.once('data', function(data) {
+            const response = JSON.parse(data);
+            saved = response.data;
+
+            assert.ok(saved._id);
+            done();
+        });
+
+        client.write(JSON.stringify(message));
+    });
+
+});
