e540c1e743684e315c5d01503df311319f35a730
Brigitte Huneke <brigittehuneke@gmail.com>
2017-02-04 15:49:04 -0800

    adjusted directory

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const server = require('./lib/db-server');
 
 const PORT = 65000;
-const TEST_DIR = '../test/test-dir-create';
+const TEST_DIR = './test/test-dir-create';
 
 server.start({
     baseDir: TEST_DIR,
