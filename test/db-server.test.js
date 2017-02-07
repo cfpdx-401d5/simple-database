ad24fa381db27c0ffa3d811f644b5f709c983662
Brigitte Huneke <brigittehuneke@gmail.com>
2017-02-07 09:27:17 -0800

    added tests for db-server

--- test/db-server.test.js
+++ test/db-server.test.js
@@ -1,13 +1,14 @@
 const assert = require('assert');
 const net = require('net');
 const server = require('../lib/db-server');
-const mkdirp = require('mkdirp');
 const rimraf = require('rimraf');
+const path = require('path');
 
 const PORT = 65000;
-const TEST_DIR = './test/test-dir-create';
+const TEST_DIR = './test/test-db-server';
 
 describe('db server', () => {
+
     before(done => {
         rimraf(TEST_DIR, () => done());
     });
@@ -18,11 +19,6 @@ describe('db server', () => {
             port: PORT },
         () => { done();}
         );
-    });
-
-    after( done => {
-        client.end(done);
-        server.stop();
     });
 
     let client;
@@ -46,6 +42,7 @@ describe('db server', () => {
         };
         
         client.once('data', function(data) {
+
             const response = JSON.parse(data);
             saved = response.data;
 
@@ -54,6 +51,94 @@ describe('db server', () => {
         });
 
         client.write(JSON.stringify(message));
+    });
+
+    it('client gets data', done => {
+        const message = {
+            method: 'get',
+            table: 'names',
+            data: {
+                _id: saved._id
+            }
+        };
+
+        client.once('data', data => {
+            const response = JSON.parse(data);
+            const got = response.data;
+
+            assert.deepEqual(got._id, saved._id);
+
+            done();
+        });
+
+        client.write(JSON.stringify(message));
+
+    });
+
+    it('client gets all data', done => {
+        const message = {
+            method: 'getAll',
+            directory: TEST_DIR
+        };
+
+        client.once('data', data => {
+            const response = JSON.parse(data);
+            const all = response.data;
+            assert.deepEqual(all, [saved])
+
+            done();
+        });
+
+        client.write(JSON.stringify(message));
+    });
+
+    it('client updates data', done => {
+        const message = {
+            method: 'update',
+            table: 'names',
+            data: {
+                name: 'ben',
+                _id: saved._id
+            }
+        };
+
+        client.once('data', function(data) {
+            const response = JSON.parse(data);
+            const got = response.data;
+
+            assert.deepEqual(got._id, saved._id);
+            assert.notEqual(got.name, saved.name);
+
+            done();
+        });
+
+        client.write(JSON.stringify(message));
+    });
+
+    it('client removes data', done => {
+        const message = {
+            method: 'remove',
+            table: 'names',
+            data: {
+                _id: saved._id
+            }
+        };
+        
+        client.once('data', data => {
+            const response = JSON.parse(data);
+            const got = response.data;
+
+            assert.deepEqual(got, 1);
+
+            done();
+        });
+
+        client.write(JSON.stringify(message));
+    });
+
+    after( done => {
+        client.end(done);
+        server.stop();
     });
 
 });
