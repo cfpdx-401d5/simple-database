80a798bf57f6ab45576cc4af4a2eeebfabb27203
Clarefollett <chlaw101@gmail.com>
2017-02-18 15:13:36 -0800

    test passing

--- test/simple-db-test.js
+++ test/simple-db-test.js
@@ -5,60 +5,46 @@ const simpleid = require('simpleid');
 
 const simpleDb = require('../lib/simple-db');
 
-const db = simpleDb.create('./test-dir');
+const test = './test/db-test-dir';
 
-const testObject = {
-    name: 'test'
-};
+var db;
 
-const removeTestObject = {
-    name: 'remove'
-};
+var testOne = {
+    message: 'testing'
+}
+var testTwo = {
+    message: 'hello'
+}
+var testThree = {
+    message: 'yo'
+}
+var testFour = {
+    message: 'tesing 123'
+}
+var testFive = {
+    message: 'last one'
+}
 
 
 before(done => {
-    rimraf('./test/created-dir', () => done());
+    db = simpleDb.create(test);
+    fs.stat(test, (err, stats) => {
+        assert.equal(null, err);
+        done();
+    });
+});
+
+after(done => {
+    rimraf(test, [], (err) => {
+        done();
+    });
 });
 
 describe('creates and deletes directory', () => {
-    
-    it('checks if directory is created', function() {
-        simpleDb.create('./test/created-dir');
-        fs.readdir('./test/created-dir', (err, files) => {
-            if(err) return done(err);
-            assert.equal(files, []);
-
+    it('saves an object and assigns id', (done) => {
+        db.save('hello', testOne, (err, result) => {
+            assert.equal(testOne.message, 'testing');
             done();
-        });
-    });
-    
-    it('deletes the directory or actually checks to see if it exists', () => {
-        fs.readdir('./test/created-dir', (err, files) => {
-            assert.deepEqual(err.code, 'ENOENT');
-
-            done();
-        });
-    });
-});
-
-describe('saves, updates, gets, gets all, and removes file', function() {
-    
-    it('saves file', done => {
-        db.save('./test/created-dir/index.txt', testObject, (err, object) => {
-            if(err) return done(err);
-            assert.equal(object.name, testObject.name);
-            done();
-        });
-    });
-    
-    it('updates file', done => {
-        testObject.name = 'hello';
-        db.update('./test/created-dir', testObject, (err, object) => {
-            if(err) return done(err);
-            assert.deepEqual(object.name, testObject.name);
-            done();
-        });
-
-        testObject.name = 'test';
+        })
     });
 });
