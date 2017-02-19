23f6c343ae5810064b8cad23f17d820f5d4b91ba
Clarefollett <chlaw101@gmail.com>
2017-02-19 13:28:08 -0800

    setting up for tests

---
+++ test/db-server.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const net = require('net');
+const rimraf = require('rimraf');
+
+const server = require('../lib/simple-db');
+
+const PORT = 65000;
+const DATA = './test/data';
+
+describe('database TCP server', () => {
+
+});
